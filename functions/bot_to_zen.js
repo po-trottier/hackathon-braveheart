e643db96fe12d4ae3c3d40897f1ac9799b9c9e39
NahejL <nahejlefebvre@gmail.com>
2020-01-25 13:00:17 -0500

    Created htp handler

---
+++ functions/bot_to_zen.js
@@ -0,0 +1,3 @@
+exports.handler = (req, res) => {
+    res.end('ok')
+}
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,4 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const bot = require('bot-to-zen.js')
+functions.https.onRequest(bot.handler)
