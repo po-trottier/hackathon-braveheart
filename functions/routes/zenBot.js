155f36fbfb46708ee7b2df1bfd6029c1fa7ac5c0
NahejL <nahejlefebvre@gmail.com>
2020-01-26 09:26:29 -0500

    Fixed stuf

--- functions/routes/zenBot.js
+++ functions/routes/zenBot.js
@@ -22,8 +22,6 @@ express.use('/',  async ({body}, res, next) => {
     await logs.doc('BODY').set(body)
     
     try{
-        let text
-        if(body.messages && body.messages[0]) text = body.messages[0].text
         switch(body.trigger){
             case "delivery:success":
             await smooch.appUsers.sendMessage(body.appUser._id, {
@@ -39,8 +37,13 @@ express.use('/',  async ({body}, res, next) => {
                     role: 'appMaker'
                 })
             break;
-            case "message:appUser":
+            case "message:appUser": {
+                let text = ""
+                if(body.messages && body.messages[0]) text = body.messages[0].text
+                let isThanks = false
+                
                 if(text.includes('thank')){
+                    isThanks = true
                     await smooch.appUsers.sendMessage(body.appUser._id, {
                         type: 'text',
                         text: `You're welcome ${body.appUser.givenName}. What else can I do for you ?`,
@@ -50,7 +53,7 @@ express.use('/',  async ({body}, res, next) => {
                 await ( async () => {//its dum but only for eslint to shut up
                     const result = JSON.parse(await request(`https://us-central1-braveheart-265cb.cloudfunctions.net/keywords/?message=${text}`))
                     await smooch.appUsers.sendMessage(body.appUser._id, {
-                        text: result.isDefined ? 'Here\'s a little something to pump you up !' : 'Sorry, I did not understand what you ment. So here\'s our to pick!',
+                        text: result.isDefined || isThanks ? 'Here\'s a little something to pump you up !' : 'Sorry, I did not understand what you meant. So here\'s our pick!',
                         role: 'appMaker',
                         type: 'text',
                         actions: [
@@ -62,7 +65,8 @@ express.use('/',  async ({body}, res, next) => {
                         ]
                     })
                 })()
-            break;
+                break;
+            }
         }    
     }
     catch(e){
